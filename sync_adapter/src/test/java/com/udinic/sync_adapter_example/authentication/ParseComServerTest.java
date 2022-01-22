569744bf91daeae4319b74c80f04f89702991792
TonyTandAndroid <tonythompsoncmu@gmail.com>
2022-01-22 05:03:40 -0800

    Support gradle build system.

---
+++ sync_adapter/src/test/java/com/udinic/sync_adapter_example/authentication/ParseComServerTest.java
@@ -0,0 +1,10 @@
+package com.udinic.sync_adapter_example.authentication;
+
+import static org.junit.Assert.*;
+
+public class ParseComServerTest {
+
+  @org.junit.Test
+  public void getAppParseComHeaders() {
+  }
+}
